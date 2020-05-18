5af91edae6a82dfc81772ae3ea481d4bffdf7d99
root <root@b6ae7926cecd>
2020-05-18 23:59:21 +0000

    Finished TashMe and RobotSquare but working on RobotSpirl

--- src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
+++ src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
@@ -12,13 +12,20 @@ public class RobotSpiral {
 	public static void main(String[] args) {
 		
 		// Create a new Robot
+		Robot rob = new Robot();
 		
 		// Set your robot's pen down 
-		
+		//rob.penDown();
 		// SPEED. Set the robot to go at max speed (100)
+		rob.setSpeed(100);
 		
         // LOOP. Use a for loop to repeat ALL the following lines of code 50 times. 
+		for(int i = 0; i < 50; i+= 1) {
+			rob.move(5*i);
+			rob.turn(360/7);
+			
 		
+		}
 				// Change the robot pen color to random
 			
 				// DRAW.    Move the robot (5*i) pixels
